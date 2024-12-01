f50276e3912deb9b13c083127e3c62178746ccec
JJonasB <43815203+JJonasB@users.noreply.github.com>
2024-12-01 13:06:11 +0800

    Reapply "Login - Logout System fully implemented"
    This reverts commit 63ba0c9401cfbaa3f583c71b7c4ac1f9ceb925aa.

--- #Navbar.php
+++ #Navbar.php
@@ -34,7 +34,7 @@
                     echo "<div class='bx bx-menu' id='menu-icon'>";
                 } else {
                     echo "<a href='#' class='cartcon'> <i class='ri-shopping-cart-2-fill'> </i> Cart</a>";
-                    echo "<a href='#' class=''> <i class='ri-user-fill'> </i>Profile</a>";
+                    echo "<a href='#' class=''> <i class='ri-user-fill'> </i>". htmlspecialchars($_SESSION['username'])."</a>";
                     echo "<a href='Log-out.php' class=''> </i>Logout</a>";
                     echo "<div class='bx bx-menu' id='menu-icon'>";
                 }
