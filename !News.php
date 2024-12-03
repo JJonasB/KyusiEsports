407f014a1b03e0d526a473937263c555527375ad
Jemz-h <jemzflips@gmail.com>
2024-12-04 05:23:55 +0800

    Add files via upload

--- !News.php
+++ !News.php
@@ -9,89 +9,88 @@
 </head>
 
 <body>
-    <?php 
-        include "functions/conn.php";
-        $sql = "SELECT Text FROM adminedit ORDER BY ID DESC LIMIT 1"; 
-        $result = $conn->query($sql);
-        
-        if ($result && $row = $result->fetch_assoc()) {
-            $latestMessage = htmlspecialchars($row['Text']); 
-        } else {
-            $latestMessage = "No messages found."; 
-        }
-    ?>
 <div class="wrapper">
-        
-        <!-- Header Section -->
+        <!-- Navbar -->
         <?php include "#Navbar.php"; ?>
 
-        <!-- Main Body Section sa NEWS -->
+        <!-- About Us Main Body -->
     <main>
-        <p class="titlenews">N<span style="color: #CD2042; font-family: Oswald;">E</span>WS</p>
+    <p class="titlenews">N<span class="E">E</span>WS</p>
 
-        <div class="twocontainer">
-            <div class="item1">
-                <ul class="item1-whole-content">
-                    <li class="item1-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
-                    <li class="item1-content">Quisque tristique justo ut ex mattis interdum.</li>
-                    <li class="item1-content">Morbi suscipit lorem eget pellentesque ullamcorper.</li>
-                    <li class="item1-content">Vivamus tincidunt sapien nec condimentum venenatis.</li>
-                    <li class="item1-content">Fusce non nisi non lacus finibus dapibus.</li>
-                    <li class="item1-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
-                    <li class="item1-content">Quisque tristique justo ut ex mattis interdum.</li>
-                    <li class="item1-content">Morbi suscipit lorem eget pellentesque ullamcorper.</li>
-                    <li class="item1-content">Vivamus tincidunt sapien nec condimentum venenatis.</li>
-                    <li class="item1-content">Fusce non nisi non lacus finibus dapibus.</li>
-                    <li class="item1-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
-                    <li class="item1-content">Quisque tristique justo ut ex mattis interdum.</li>  
-                </ul>
+    <div class="item2">
+        <div class="hover-container"> <!-- New hover container -->
+            <p class="nit1">INFINIX UNIVERSITY LEAGUE TOURNAMENT<br>
+            SEASON 1</p>
+
+            <div class="news-items1">
+                <img src="qceimages/a2.jpg" alt="Tournament Image">
+                <img style="width: 40%" src="qceimages/a1.jpg" alt="Second Tournament Image">
             </div>
-        
-            <div class="item2">
-                <div class="img-content" style="display: flex;">
-                <img class="astreusimg" src="qceimages/astreus.jpg">
-                    <div class="h1h2">
-                    <p class="astreus1">
-                        <span class="a" style="font-size: #ff0909; 
-                                            font-family: Oswald;">A</span>ST<span style="color: #000000; 
-                                                                                            font-family: Oswald;">RE</span>US</p>
-                    
-                    <p class="baba-ng-astreus">
-                    <?php echo $latestMessage; ?>
-                    </p>
-                    </div>
-                </div>
-                
-                <p class="long-content">
-                    <br><br><br><br><br><br><br><br><br><br>
-                    Dito yung news mga kaibigan!! 
-                    Etiam accumsan ex et ipsum scelerisque, ac cursus nunc mollis. 
-                    Mauris volutpat pretium odio sit amet ullamcorper. Donec non tincidunt nunc. 
-                    Vestibulum at quam blandit, viverra tortor vel, mattis velit. 
-                    Pellentesque consectetur ipsum mauris, vitae ornare turpis facilisis eu. 
-                    Vivamus euismod arcu nec orci accumsan suscipit. Vestibulum ante ipsum primis 
-                    in faucibus orci luctus et ultrices posuere cubilia curae; Vivamus ac mattis ante, 
-                    quis sodales felis. Fusce nec enim a leo fringilla ornare vel sed nisi. 
-                    Nullam ut diam sed nisi tristique ultricies non eu eros. Aliquam venenatis elit 
-                    non nisl lacinia, vel pharetra dolor molestie.
-                    Phasellus eget leo eu justo dapibus pulvinar. Maecenas sit amet neque et turpis 
-                    fringilla rhoncus vel non nulla. Nulla vitae tellus in lacus ultrices ultrices. 
-                    Duis venenatis rhoncus mi. Nam odio ligula, volutpat nec velit a, viverra 
-                    ullamcorper orci. Suspendisse sapien erat, ornare ac leo et, eleifend molestie 
-                    sapien. Pellentesque et scelerisque elit. Praesent vitae neque condimentum, 
-                    posuere lorem vitae, auctor eros. Class aptent taciti sociosqu ad litora torquent 
-                    per conubia nostra, per inceptos himenaeos. Morbi lobortis, massa eu elementum 
-                    vestibulum, tellus urna ornare lectus, quis lobortis mauris felis non ante. Donec 
-                    sapien lorem, venenatis sed nisl eget, ultricies accumsan est. Donec tempor rutrum 
-                    augue, vitae commodo mauris congue et. Aliquam sed fringilla tellus. Aliquam at 
-                    lobortis libero. Maecenas eu condimentum neque, eget accumsan leo.
-                </p>
-            </div>
+
+            <p class="nit1-content">KE Tiddles, MLBB Team representatives from the 
+                Quezon City University competed on an intense battle against 
+                PUP - Pylon Mittsu on June 29, 2024 at SM Megamall and claimed the 
+                first place for their first time competing on a big stage</p>
+        </div> <!-- End of hover-container -->
+
+        <div class="hover-container">
+        <p class="nit2">INFINIX 3V3 COMMUNITY TOURNAMENTS</p>
+        <div class="news-items2">
+            <img src="qceimages/c1.jpg" alt="Group Image">
+            <p class="nit2-content">Kyusi Esports Community competed on a 3v3 
+            battle last July 12, 2024 at the Infinix Concept Store at SM 
+            North Edsa, Annex Bldg. Few of the contestants from the community 
+            won the championship with a smart watch as their prize.</p>
         </div>
-    </main>
+        </div>
 
+        <div class="hover-container">
+        <p class="nit3">Freshie Walk</p>
+        <div class="news-items3">
+            <img src="qceimages/d1.jpg" alt="Tao Image">
+            <img src="qceimages/d2.jpg" alt="Ufo Image">
+        </div>
+        <p class="nit3-content">KE Tiddles, MLBB Team representatives from the 
+            Quezon City University competed on an intense battle against 
+            PUP - Pylon Mittsu on June 29, 2024 at SM Megamall and claimed the 
+            first place for their first time competing on a big stage</p>
+        </div>
+
+        <div class="hover-container">
+        <p class="nit4">Astreus</p>
+        <div class="news-items4">
+            <img src="qceimages/e1.jpg" alt="Group Image">
+            <p class="nit4-content">KE Astreus, the League of 
+                Legends: Wildrift representatives of the Kyusi Esports Community 
+                competed on both AkoSi Dogie’s #DayoSerye and AcadArena’s 
+                October Aces: Wildrift last October 28 - 30, 2024.</p>
+        </div>
+        </div>
+
+        <div class="hover-container">
+        <p class="nit5">INFINIX UNIVERSITY LEAGUE TOURNAMENT SEASON 2</p>
+        <div class="news-items5">
+            <img src="qceimages/f1.jpg" alt="Tahh Image">
+            <img style="width: 45%" src="qceimages/f2.png" alt="Second Tournament Image">
+        </div>
+        <p class="nit5-content">KE Tiddles, MLBB Team representatives from the 
+            Quezon City University competed on an intense battle against 
+            PUP - Pylon Mittsu on June 29, 2024 at SM Megamall and claimed the 
+            first place for their first time competing on a big stage</p>
+
+        </div>
+
+
+
+
+
+    </div>
+
+    </main> 
+        
         <!-- Footer -->
         <?php include "#Footer.php"; ?>
+
 </div>
 
     <!-- External JavaScript -->
